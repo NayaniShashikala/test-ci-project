a0d8d6a3f1fd09e28d625c1fb802af6e77ca73cf
NayaniShashikala <56906401+NayaniShashikala@users.noreply.github.com>
2023-09-24 13:34:33 +0530

    added calculator unit test

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,18 @@
+public class Calculator {
+
+    public float addTwoNumbers(float num1, float num2) {
+        return num1 + num2;
+    }
+
+    public float subtractTwoNumbers(float num1, float num2) {
+        return num1 - num2;
+    }
+
+    public float multiplyTwoNumbers(float num1, float num2) {
+        return num1 * num2;
+    }
+
+    public float divideTwoNumbers(float num1, float num2) {
+        return num1 / num2;
+    }
+}
---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,26 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class CalculatorTest {
+
+    Calculator calculator = new Calculator();
+    @Test
+    public void testAdd() {
+        Assertions.assertEquals(6, calculator.addTwoNumbers(4,2));
+    }
+
+    @Test
+    public void testSubtract() {
+        Assertions.assertEquals(2, calculator.subtractTwoNumbers(4,2));
+    }
+
+    @Test
+    public void testMultiply() {
+        Assertions.assertEquals(8, calculator.multiplyTwoNumbers(4,2));
+    }
+
+    @Test
+    public void testDivide() {
+        Assertions.assertEquals(2, calculator.divideTwoNumbers(4,2));
+    }
+}
